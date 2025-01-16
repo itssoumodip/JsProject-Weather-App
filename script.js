219d16b3b6860920f7f78ae14baca1e41c2477e0
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-16 12:46:41 +0000

    api call

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const apiKey = "fd51c00c6d4faa068f4c21a163773b6c";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bengalore";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=west bengal";
 
 async function checkWeather() {
     const response = await fetch (apiUrl + `&appid=${apiKey}`);
     var data = await response.json();
     console.log(data);
+    document.querySelector('.city').innerHTML = data.name;
+    document.querySelector('.temp').innerHTML = data.main.temp + "°C";
+    document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
+
 }
+
+checkWeather();
