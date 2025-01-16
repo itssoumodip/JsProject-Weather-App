85f1e76890c4e50ff811b449dc2a65d6b17af857
Soumodip Das <soumodipddas2005@gmail.com>
2025-01-16 13:24:21 +0000

    updating the js file

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const apiKey = "fd51c00c6d4faa068f4c21a163773b6c";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=west bengal";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const response = await fetch (apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector('.search input');
+const searchBtn = document.querySelector('.search button');
+const weatherIcon = document.querySelector('.weather-icon');
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
+
     console.log(data);
+
     document.querySelector('.city').innerHTML = data.name;
-    document.querySelector('.temp').innerHTML = data.main.temp + "°C";
+    document.querySelector('.temp').innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector('.humidity').innerHTML = data.main.humidity + "%";
     document.querySelector('.wind').innerHTML = data.wind.speed + " km/h";
 
+    if (data.weather[0].main == 'Clouds')
+        weatherIcon.src = "images/clouds.png";
+    else if (data.weather[0].main == 'Clear')
+        weatherIcon.src = "images/clear.png";
+    else if (data.weather[0].main == 'Rain')
+        weatherIcon.src = "images/rain.png";
+    else if (data.weather[0].main == 'Drizzle')
+        weatherIcon.src = "images/drizzle.png";
+    else if (data.weather[0].main == 'Mist')
+        weatherIcon.src = "images/mist.png";
+
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+
